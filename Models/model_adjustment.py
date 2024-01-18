7fa2ae87f9ab9e06822345428977067a8d214cf9
Tesla2000 <fratajczak124@gmail.com>
2024-01-18 20:10:34 +0100

    Added to device in a right place

--- Models/model_adjustment.py
+++ Models/model_adjustment.py
@@ -27,7 +27,7 @@ def _window_forward_wrapper(forward, window_size: int, window_stride: int):
                         )
                     )
                 )
-            )
+            ).to(device)
             windows = forward(windows)
             mean_windows = torch.mean(windows)
             results[index] = torch.sigmoid(mean_windows)
